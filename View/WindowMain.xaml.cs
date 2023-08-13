371fc14366ed560564971d416c259e5b2aeb226d
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-08-13 08:40:15 -0600

    chore: fix awful tab formatting in WindowMain.xaml.cs

--- View/WindowMain.xaml.cs
+++ View/WindowMain.xaml.cs
@@ -3,33 +3,31 @@ using System.Windows;
 using System.Windows.Media.Effects;
 
 namespace SJB.WPF3D
-	{
-	public partial class WindowMain : Window
-		{
-		public WindowMain()
-			{
-			InitializeComponent();
-			}
+{
+  public partial class WindowMain : Window
+  {
+    public WindowMain()
+    {
+      InitializeComponent();
+    }
 
-		private void ButtonClick_Planet(object sender, RoutedEventArgs e)
-			{
+    private void ButtonClick_Planet(object sender, RoutedEventArgs e)
+    {
+    }
 
-			}
+    private void ButtonClick_About(object sender, RoutedEventArgs e)
+    {
+      // this.Effect = new BlurEffect() { Radius = 10 };
+      WindowAbout winAbout = new WindowAbout()
+      {
+        Owner = this,
+        Icon = this.Icon,
+        BorderBrush = this.BorderBrush,
+        Background = this.Background
+      };
 
-		private void ButtonClick_About(object sender, RoutedEventArgs e)
-			{
-			//this.Effect = new BlurEffect() { Radius = 10 };
-
-			WindowAbout winAbout = new WindowAbout()
-																		{
-																		Owner = this,
-																		Icon = this.Icon,
-																		BorderBrush = this.BorderBrush,
-																		Background = this.Background
-																		};
-
-			winAbout.ShowDialog();
-			//this.Effect = null;
-			}		
-		}
-	}
+      winAbout.ShowDialog();
+      // this.Effect = null;
+    }
+  }
+}
