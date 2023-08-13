2654cd861fa652ebbcf52cb0b32376b888754145
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-08-13 08:45:38 -0600

    chore: fix tab formatting in App.xaml.cs

--- Application/App.xaml.cs
+++ Application/App.xaml.cs
@@ -5,37 +5,37 @@ using System.Collections.Generic;
 using Microsoft.VisualBasic.ApplicationServices;
 
 namespace SJB.WPF3D
-	{
-	public class ApplicationEntry
-		{
-		[STAThread]
-		public static void Main(String[] args)
-			{
-			SingleInstanceApp singleinstanceapp = new SingleInstanceApp();
-			singleinstanceapp.Run(args);
-			}
-		}
-	
+{
+  public class ApplicationEntry
+  {
+    [STAThread]
+    public static void Main(String[] args)
+    {
+      SingleInstanceApp singleinstanceapp = new SingleInstanceApp();
+      singleinstanceapp.Run(args);
+    }
+  }
+
 	public class SingleInstanceApp : WindowsFormsApplicationBase
-		{
-		private Application app;
+  {
+    private Application app;
 
-		public SingleInstanceApp()
-			{ this.IsSingleInstance = true; }
+    public SingleInstanceApp()
+      { this.IsSingleInstance = true; }
+  }
 
-		protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs e)
-			{
-			app = new Application();
-			WindowMain appWindowMain = new WindowMain();			
-			
-			app.Run(appWindowMain);
-			return false;
-			}
+  protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs e)
+  {
+    app = new Application();
+    WindowMain appWindowMain = new WindowMain();			
 
-		protected override void OnStartupNextInstance(StartupNextInstanceEventArgs e)
-			{
-			e.BringToForeground = true;
-			app.MainWindow.Activate();
-			}
-		}
-	}
+    app.Run(appWindowMain);
+    return false;
+  }
+
+  protected override void OnStartupNextInstance(StartupNextInstanceEventArgs e)
+  {
+    e.BringToForeground = true;
+    app.MainWindow.Activate();
+  }
+}
